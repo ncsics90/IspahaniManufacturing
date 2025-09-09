dd5903ee084ad8f6dbd3727086383644a77a237d
ncsics90 <ncsubasinghe@gmail.com>
2025-09-09 15:40:32 +0530

    Globel File Commented fully

--- global-setup.js
+++ global-setup.js
@@ -1,25 +1,25 @@
-// global-setup.js
-import { chromium } from '@playwright/test';
-import { login_page } from './pages/loginPage';
-import { loginData } from './data/logindata';
-import { login } from './actions/loginActions';
+// // global-setup.js
+// import { chromium } from '@playwright/test';
+// import { login_page } from './pages/loginPage';
+// import { loginData } from './data/logindata';
+// import { login } from './actions/loginActions';
 
-async function globalSetup() {
-  // Launch browser
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  const page = await context.newPage();
+// async function globalSetup() {
+//   // Launch browser
+//   const browser = await chromium.launch({ headless: false });
+//   const context = await browser.newContext();
+//   const page = await context.newPage();
 
-  // Navigate to login and perform login
-  const Login = new login_page(page);
-  await Login.navigateUrl(loginData.baseUrl);
-  await login(page, Login, loginData.username, loginData.password);
+//   // Navigate to login and perform login
+//   const Login = new login_page(page);
+//   await Login.navigateUrl(loginData.baseUrl);
+//   await login(page, Login, loginData.username, loginData.password);
 
-  // Save storage state for all tests to reuses
-  await context.storageState({ path: 'storageState.json' });
+//   // Save storage state for all tests to reuses
+//   await context.storageState({ path: 'storageState.json' });
 
-  // Close browser after setup
-  await browser.close();
-}
+//   // Close browser after setup
+//   await browser.close();
+// }
 
-export default globalSetup;
+// export default globalSetup;
